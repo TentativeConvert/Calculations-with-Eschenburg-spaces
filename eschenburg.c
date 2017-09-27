bdfa5eba815ef729ba85d14b88e3dc6c9e80ebf7
Marcus Zibrowius <marcus.zibrowius@cantab.net>
2017-09-27 11:59:41 +0200

    Use (long long) instead of (int) for some computations.

--- eschenburg.c
+++ eschenburg.c
@@ -16,10 +16,10 @@
 #include <boost/multiprecision/cpp_dec_float.hpp>
 //////////////////////////////////////////////////
 // Auxiliary mathematics:
-int square(int a){ return a*a; }
+// should use templates or libary ...
+long long square(long long a){ return a*a; }
 int absolute (int a){ return a >= 0 ? a : -a; }
-int maximum (int a, int b){ return a > b ? a : b; }
-int minimum (int a, int b){ return a < b ? a : b; }
+long long absolute (long long a){ return a >= 0 ? a : -a; }
 int signed_mod (int a, int base)
 // In general, sign of % is machine dependent, 
 // see https://stackoverflow.com/a/4003287/3611932.
@@ -36,9 +36,25 @@ int signed_mod (int a, int base)
     remainder += base;
   return remainder;
 }
+long long signed_mod (long long a, long long base)
+{
+  long long remainder = a % base;
+  if(2*remainder > base)  // remainder > base/2, in "integer language"
+    remainder -= base;
+  else if(2*remainder <= -base) // remainder < -base/2, in "integer language"
+    remainder += base;
+  return remainder;
+}
 int absolute_mod (int a, int base)
 {
   int remainder = a % base;
+  if(remainder < 0)
+    remainder += absolute(base);
+  return remainder;
+}
+long long absolute_mod (long long a, long long base)
+{
+  long long remainder = a % base;
   if(remainder < 0)
     remainder += absolute(base);
   return remainder;
@@ -56,7 +72,7 @@ int absolute_mod (int a, int base)
     }
   return absolute(a);
   }*/
-boost::rational<int> reduce_mod_ZZ(boost::rational<int> q)
+boost::rational<long long> reduce_mod_ZZ(boost::rational<long long> q)
 {
   // Input:   q  in QQ 
   // Output:  q' in (-1/2, 1/2] in QQ such that 
@@ -75,7 +91,7 @@ boost::rational<int> reduce_mod_ZZ(boost::rational<int> q)
 //////////////////////////////////////////////////
 // Lens space invariants
 
-boost::rational<int> lens_s2(int p, int param[4])
+boost::rational<long long> lens_s2(int p, int param[4])
 {
   boost::multiprecision::cpp_dec_float_100 a = 0;
   for(int k = 1; k < absolute(p); ++k)
@@ -93,10 +109,10 @@ boost::rational<int> lens_s2(int p, int param[4])
   std::cout << std::setprecision(std::numeric_limits<boost::multiprecision::cpp_dec_float_100>::max_digits10)
 	    << a*45 
 	    << std::endl; // print a
-  int rounded_45_a = (int)boost::math::round(a*45);
-  printf("     round(...) = %d\n",rounded_45_a);
-  boost::rational<int> s2(rounded_45_a,45*16*p);
-  //// printf("     s2 = %d/%d\n",s2.numerator(),s2.denominator());
+  long long rounded_45_a = (long long)boost::math::round(a*45);
+  printf("     round(...) = %lld\n",rounded_45_a);
+  boost::rational<long long> s2(rounded_45_a,45*16*p);
+  //// printf("     s2 = %lld/%lld\n",s2.numerator(),s2.denominator());
   return s2;
 }
 
@@ -109,15 +125,15 @@ struct Space {
   int mr; // mr = -r = "minus r"
   int s;
   int p1;
-  boost::rational<int> s22;
-  boost::rational<int> s2;
+  boost::rational<long long> s22;
+  boost::rational<long long> s2;
   int good_row; // for "condition C"
   int good_col; // for "condition C"
 
   void print(FILE *file){
-    printf(         "[%4d,%4d,%4d |%4d,%4d,%4d] --> r = %4d, s = %4d, p1 = %4d, s22 = %d/%d, s2 = %d/%d\n",
+    printf(         "[%4d,%4d,%4d |%4d,%4d,%4d] --> r = %4d, s = %4d, p1 = %4d, s22 = %lld/%lld, s2 = %lld/%lld\n",
 		    k[0],k[1],k[2],l[0],l[1],l[2],-mr,s,p1,s22.numerator(),s22.denominator(),s2.numerator(),s2.denominator());
-    fprintf(  file, "[%4d,%4d,%4d |%4d,%4d,%4d] --> r = %4d, s = %4d, p1 = %4d, s22 = %d/%d, s2 = %d/%d\n",
+    fprintf(  file, "[%4d,%4d,%4d |%4d,%4d,%4d] --> r = %4d, s = %4d, p1 = %4d, s22 = %lld/%lld, s2 = %lld/%lld\n",
 	      k[0],k[1],k[2],l[0],l[1],l[2],-mr,s,p1,s22.numerator(),s22.denominator(),s2.numerator(),s2.denominator());
   }
   void compute_r_s_p1(void){
@@ -142,11 +158,11 @@ struct Space {
     if(boost::math::gcd(k[0] - l[2], k[1] - l[1]) > 1) return false;
     // Is the space positively curved? 
     // -- Test conditions of [CEZ06] (1.2):
-    int min_l = minimum(l[0],minimum(l[1],l[2]));
+    int min_l = std::min(l[0],std::min(l[1],l[2]));
     if (k[0] == min_l) return false;
     if (k[1] == min_l) return false;
     if (k[2] == min_l) return false;
-    int max_l = maximum(l[0],maximum(l[1],l[2]));
+    int max_l = std::max(l[0],std::max(l[1],l[2]));
     if (k[0] == max_l) return false;
     if (k[1] == max_l) return false;
     if (k[2] == max_l) return false;
@@ -166,7 +182,6 @@ struct Space {
       if(gcdA(0,c,1,c) == 1 && gcdA(0,c,2,c) == 1 && gcdA(1,c,2,c) == 1)
 	{
 	  good_col = c;
-	  //// printf("Column %d is good: (%d,%d,%d)\n",c+1,k[0]-l[c],k[1]-l[c],k[2]-l[c]);
 	  return;
 	}
     }
@@ -174,7 +189,6 @@ struct Space {
       if(gcdA(r,0,r,1) == 1 && gcdA(r,0,r,2) == 1 && gcdA(r,1,r,2) == 1)
 	{
 	  good_row = r;
-	  printf("using row %d\n", r+1);
 	  return;
 	}
     }
@@ -189,41 +203,43 @@ struct Space {
     //     s22 = 2|r|s2
     //
     int jp1 = absolute_mod(j+1,2);
-    //// printf("I'm using column %d\n",j+1);
+    printf("I'm using column %d: ",j+1);
+    printf("(%d,%d,%d)\n",k[0]-l[j],k[1]-l[j],k[2]-l[j]);
     //// printf("jp1 = %d",jp1+1);
-    int q = 
+    long long q = 
       square(k[0]-l[j])   + square(k[1]-l[j])   + square(k[2]-l[j]) +
       square(k[0]-l[jp1]) + square(k[1]-l[jp1]) + square(k[2]-l[jp1])
       - square(l[j]-l[jp1]);
-    int d = 16*3*(-mr)*(k[0]-l[j])*(k[1]-l[j])*(k[2]-l[j]);
+    long long d = 16*3*(long long)(-mr)*(k[0]-l[j])*(k[1]-l[j])*(k[2]-l[j]);
     s2.assign(q-2,d);
-    //// printf("q = %d, d = %d\n", q, d);
-    //// printf("=> (q-2)/d = %d/%d\n",s2.numerator(),s2.denominator());
+    printf("q = %lld, r = %lld, d = %lld\n", q, (long long)(-mr), d);
+    printf("=> (q-2)/d = %lld/%lld\n",s2.numerator(),s2.denominator());
     for(int i = 0; i < 3; ++i)
       {
-	//// printf("Lens space invariant s_2 for i=%d: \n",i+1);
+	printf("Lens space invariant s_2 for i=%d: \n",i+1);
 	int ip1 = absolute_mod(i+1,3);
 	int ip2 = absolute_mod(i+2,3);
 	int params[4] = {k[ip1]-l[j], k[ip2]-l[j], k[ip1]-l[jp1], k[ip2]-l[jp1]};
-        //// printf("     parameters: %d; %d, %d, %d, %d\n", k[i]-l[j], params[0], params[1], params[2], params[3]);
+        printf("    parameters: %d; %d, %d, %d, %d\n", k[i]-l[j], params[0], params[1], params[2], params[3]);
 	s2 += -lens_s2(k[i]-l[j], params);
       }
     s2 = reduce_mod_ZZ(s2);
-    //// printf("=> s2(E)  = %d/%d\n",s2.numerator(),s2.denominator());
+    printf("=> s2(E)  = %lld/%lld\n",s2.numerator(),s2.denominator());
   }
   void compute_s2_row(int j)
   {
     int jp1 = absolute_mod(j+1,2);
-    //// printf("I'm using row %d\n",j+1);
+    printf("I'm using row %d",j+1);
+    printf(" (%d,%d,%d)\n",k[j]-l[0],k[j]-l[1],k[j]-l[2]);
     //// printf("jp1 = %d",jp1+1);
-    int q = 
+    long long q = 
       square(k[j]-l[0])   + square(k[j]-l[1])   + square(k[j]-l[2]) +
       square(k[jp1]-l[0]) + square(k[jp1]-l[1]) + square(k[jp1]-l[2])
       - square(k[j]-k[jp1]);
-    int d = 16*3*(-mr)*(k[j]-l[0])*(k[j]-l[1])*(k[j]-l[2]);
+    long long d = 16*3*(long long)(-mr)*(k[j]-l[0])*(k[j]-l[1])*(k[j]-l[2]);
     s2.assign(q-2,d);
-    //// printf("q = %d, d = %d\n", q, d);
-    //// printf("=> (q-2)/d = %d/%d\n",s2.numerator(),s2.denominator());
+    //// printf("q = %lld, d = %lld\n", q, d);
+    //// printf("=> (q-2)/d = %lld/%lld\n",s2.numerator(),s2.denominator());
     for(int i = 0; i < 3; ++i)
       {
 	//// printf("Lens space invariant s_2 for i=%d: \n",i+1);
@@ -234,11 +250,11 @@ struct Space {
 	s2 += lens_s2(k[j]-l[i], params);
       }
     s2 = reduce_mod_ZZ(s2);
-    //// printf("=> s2(E)  = %d/%d\n",s2.numerator(),s2.denominator());
+    //// printf("=> s2(E)  = %lld/%lld\n",s2.numerator(),s2.denominator());
   }
   void compute_s2(void)
   {
-    //// printf("\n===== E(%d, %d, %d | %d, %d, %d) ===== \n", k[0], k[1], k[2], l[0], l[1], l[2]);
+    printf("\n===== E(%d, %d, %d | %d, %d, %d) ===== \n", k[0], k[1], k[2], l[0], l[1], l[2]);
     find_good_col_or_row();
     if (good_col >= 0)
 	compute_s2_col(good_col);
@@ -249,11 +265,11 @@ struct Space {
   }
   void compute_s22(void) // only possible AFTER computing s2
   {
-    //// printf("=> s2(E)  = %d/%d\n",s2.numerator(),s2.denominator());
+    //// printf("=> s2(E)  = %lld/%lld\n",s2.numerator(),s2.denominator());
     s22 = 2*absolute(mr)*s2;
-    //// printf("=> s22(E) = %d/%d (in QQ)\n",s22.numerator(),s22.denominator());
+    //// printf("=> s22(E) = %lld/%lld (in QQ)\n",s22.numerator(),s22.denominator());
     s22 = reduce_mod_ZZ(s22);
-    //// printf("=> s22(E) = %d/%d (in QQ/ZZ)\n",s22.numerator(),s22.denominator());
+    //// printf("=> s22(E) = %lld/%lld (in QQ/ZZ)\n",s22.numerator(),s22.denominator());
   }
 };
 
@@ -274,15 +290,15 @@ main(){
      float a = M_PI;
      printf("%.6f\n",a);
 
-     boost::rational<int> myr(10,6);
-     printf("%d/%d\n",myr.numerator(),myr.denominator());
-     boost::rational<int> myr2(0,6);
-     printf("%d/%d\n",myr2.numerator(),myr2.denominator());
+     boost::rational<long long> myr(10,6);
+     printf("%lld/%lld\n",myr.numerator(),myr.denominator());
+     boost::rational<long long> myr2(0,6);
+     printf("%lld/%lld\n",myr2.numerator(),myr2.denominator());
 
-     boost::rational<int> q(-49,3);
-     printf("q = %d/%d\n",q.numerator(),q.denominator());
+     boost::rational<long long> q(-49,3);
+     printf("q = %lld/%lld\n",q.numerator(),q.denominator());
      q = reduce_mod_ZZ(q);
-     printf("&q' = %d/%d\n",q.numerator(),q.denominator());
+     printf("&q' = %lld/%lld\n",q.numerator(),q.denominator());
      printf("signed_mod(-49,3) = %d\n",signed_mod(-49,3));
   */
 
@@ -352,7 +368,7 @@ main(){
 	  E2.compute_s2();
 	  E2.compute_s22();
 	  printf("\n");
-	  boost::rational<int> a_half(1,2);
+	  boost::rational<long long> a_half(1,2);
 	  if ((E1.s == E2.s && E1.s22 == E2.s22) // oriented case
 	      || (E1.s == -E2.s && (E1.s22 == -E2.s22 || E1.s22 == a_half && E2.s22 == a_half))) // non-oriented case
 	    {
@@ -393,7 +409,7 @@ main(){
 	    E1.print(file_list_basic);
 	    E2.print(file_list_basic);
 
-	    boost::rational<int> a_half(1,2);
+	    boost::rational<long long> a_half(1,2);
 	    if ((E1.s == E2.s && E1.s22 == E2.s22) // oriented case
 		|| (E1.s == -E2.s && (E1.s22 == -E2.s22 || E1.s22 == a_half && E2.s22 == a_half))) // non-oriented case
 	      {
